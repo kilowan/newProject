dfabf97576af5007feda83880a0ae3142d5101c8
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2020-12-27 17:09:44 +0100

    clean code

--- insertparte.php
+++ insertparte.php
@@ -4,43 +4,27 @@ session_start();
 <?php
 if (isset($_SESSION['loggedin']))
 {
-		//Conexion Mysql
+	//Conexion Mysql
 	$sql = json_decode($_SESSION['sql']);
 	$conexion = new mysqli($sql->host_db, $sql->user_db, $sql->pass_db, $sql->db_name);
-	//$conexion = new mysqli($_SESSION['host_db'], $_SESSION['user_db'], $_SESSION['pass_db'], $_SESSION['db_name']);
 	$user = json_decode($_SESSION['user']);
 	$dni = $user->dni;
 	$id_emp = $user->id;
-	//$dni = $_SESSION['dni'];
 	$id_part = $_POST['id_part'];
 	$not_tec = $_POST['not_tec'];
 	$pieza = $_POST['pieza'];
-	//$id_emp = $_SESSION['id_emp'];
-	//extrae id y nombre de empleado
-	$con = $conexion->query("SELECT nombre, apellido1, apellido2 FROM Empleados where dni='$dni'");
-	$fila = mysqli_fetch_assoc($con);
-	$nombre_tecnico = $fila['nombre']." ".$fila['apellido1']." ".$fila['apellido2'];	
-	//extrae notas anteriores
-	$con = $conexion->query("SELECT not_tec FROM parte WHERE id_part=$id_part");
-	$fila = mysqli_fetch_assoc($con);
-	if (mysqli_num_rows($con) > 0)
-	{
-		$not_ant = $fila['not_tec'].'<br />Actualización:<br />'.$not_tec;
-	}
-	else
-	{
-		$not_ant = $not_tec;
-	}
+	$nombre_tecnico = $user->name.' '.$user->surname1.' '.$user->surname2;
 	if($pieza == '--')
 	{
-		$con = $conexion->query("UPDATE parte  set not_tec = '$not_ant', tec_res = $id_emp, nom_tec='$nombre_tecnico' 
+		$conexion->query("UPDATE parte  set tec_res = $id_emp, nom_tec='$nombre_tecnico' 
 		WHERE id_part = $id_part and (tec_res='$id_emp' or tec_res is null) and resuelto=0");
 	}
 	else
 	{
-		$con = $conexion->query("UPDATE parte SET pieza = '$pieza', not_tec = '$not_ant', tec_res = $id_emp, nom_tec='$nombre_tecnico' 
+		$conexion->query("UPDATE parte SET pieza = '$pieza', not_tec = '$not_ant', tec_res = $id_emp, nom_tec='$nombre_tecnico' 
 		WHERE id_part = $id_part and (tec_res='$id_emp' or tec_res is null) and resuelto=0");
 	}
+	$conexion->query("INSERT INTO notes (employee, incidence, noteType, noteStr) VALUES ($id_emp, $id_part, $user->tipo, $not_tec)");
 	$_SESSION['funcion'] = 'Partes';
 	header('Location: veremp.php');	
 }
