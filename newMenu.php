cf010f7b7156c01c53b718cec4822555c31b3179
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2021-02-11 23:28:50 +0100

    fix issue

--- newMenu.php
+++ newMenu.php
@@ -111,7 +111,7 @@ include 'newFunctions.php';
     {
         header('Access-Control-Allow-Origin: *');
         header('Content-Type: application/json');
-        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: *");
         header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
         $method = $_SERVER['REQUEST_METHOD'];
